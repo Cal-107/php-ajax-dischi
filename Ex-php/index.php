b1166ed1e539629207ca26ce752b1b495de883cf
“Cal-107” <kaly___7@hotmail.it>
2021-12-21 13:17:12 +0100

    first commit

---
+++ Ex-php/index.php
@@ -0,0 +1,23 @@
+<?php
+require_once __DIR__ . '/data/database.php';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/style.css">
+    <title>Php Dischi</title>
+</head>
+<body>
+    <header>
+        <section>
+            <div class="img-box">
+                <img src="./img/logo.png" alt="Spotify Logo">
+            </div>
+        </section>
+    </header>
+</body>
+</html>
